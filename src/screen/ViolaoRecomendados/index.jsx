eea35461c314004a3f3b604f92d35cdaabfcfa50
Davim187 <daviimorais39@gmail.com>
2023-06-01 09:08:37 -0300

    Atualização de telas

--- src/screen/ViolaoRecomendados/index.jsx
+++ src/screen/ViolaoRecomendados/index.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import Header from '../../componentes/header';
 
+import CPX700 from '../../img/CPX700 – Yamaha.jpg';
+import MD18 from '../../img/4370370965_1.jpg';
+import NF14 from '../../img/eb696fd61b.jpg';
+import Ac39 from '../../img/f96fa7e9242c79dd64db7355bea269f6.jpg';
+import RX201 from '../../img/8851296610681f5f39.jpg';
+
 import './style.css';
 
 function Recomendados() {
@@ -11,73 +17,68 @@ function Recomendados() {
         <h2 class="h4"> Recomendaçoes:</h2>
         <article id="article">
           <div class="card">
-            <img class="image_container" src="/Imagens/R (3).jfif" alt="" />
+            <img class="image_container" src={CPX700} alt="CPX700" />
             <div class="text_back">
-              <h1>Lorem ipsum</h1>
-
+              <h1>CPX700 – Yamaha</h1>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sed
-                nunc vel lectus commodo aliquet ac vel velit.
-                <br /> <br />
-                Vivamus cursus a nunc vitae blandit. Duis quam justo, sagittis a
-                fringilla quis, facilisis eu mauris.
+                Os violões da série 700 distinguem-se pelo seu estilo e
+                características que foram trazidas dos violões da série 1000,
+                mas apresentam um sistema de amplificação ART exclusivo.
               </p>
             </div>
           </div>
           <div class="card">
-            <img class="image_container" src="/Imagens/R (2).jfif" alt="" />
+            <img class="image_container" src={MD18} alt="MD18" />
             <div class="text_back">
-              <h1>Lorem ipsum</h1>
+              <h1>Memphis MD18 – Tagima</h1>
 
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sed
-                nunc vel lectus commodo aliquet ac vel velit.
-                <br /> <br />
-                Vivamus cursus a nunc vitae blandit. Duis quam justo, sagittis a
-                fringilla quis, facilisis eu mauris.
+                O Memphis MD18 é um violão folk elétrico de excelente relação
+                custo x benefício. Este instrumento é quase todo fabricado em
+                lindan que é uma madeira de baixo custo mas com ótima
+                sonoridade.
               </p>
             </div>
           </div>
           <div class="card">
-            <img class="image_container" src="/Imagens//OIP (1).jfif" alt="" />
+            <img class="image_container" src={NF14} alt="NF14" />
             <div class="text_back">
-              <h1>Lorem ipsum</h1>
+              <h1>NF14 – Giannini</h1>
 
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sed
-                nunc vel lectus commodo aliquet ac vel velit.
-                <br /> <br />
-                Vivamus cursus a nunc vitae blandit. Duis quam justo, sagittis a
-                fringilla quis, facilisis eu mauris.
+                Violão Eletroacústico Giannini Start Flat Cutaway Nylon NF-14
+                Preto O Violão Elétrico Giannini Start NF-14 é um instrumento
+                com belíssimo visual e ótimo custo benefício, ideal para
+                estudantes e músicos iniciantes.
               </p>
             </div>
           </div>
 
           <div class="card">
-            <img class="image_container" src="/Imagens/R (1).jfif" alt="" />
+            <img class="image_container" src={Ac39} alt="Ac39" />
             <div class="text_back">
-              <h1>Lorem ipsum</h1>
+              <h1>AC39 Tagima Memphis </h1>
 
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sed
-                nunc vel lectus commodo aliquet ac vel velit.
-                <br /> <br />
-                Vivamus cursus a nunc vitae blandit. Duis quam justo, sagittis a
-                fringilla quis, facilisis eu mauris.
+                Um ótimo violão para quem irá começar a tocar suas primeiras
+                notas. Violão com cordas de nylon que proporcionam mais conforto
+                para os iniciantes. Com certeza o melhor violão custo benefício
+                do mercado
               </p>
             </div>
           </div>
           <div class="card">
-            <img class="image_container" src="/Imagens/OIP.jfif" alt="" />
+            <img class="image_container" src={RX201} alt="RX201" />
             <div class="text_back">
-              <h1>Lorem ipsum</h1>
+              <h1>Rx201 - Rozini</h1>
 
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sed
-                nunc vel lectus commodo aliquet ac vel velit.
-                <br /> <br />
-                Vivamus cursus a nunc vitae blandit. Duis quam justo, sagittis a
-                fringilla quis, facilisis eu mauris.
+                O Rozini RX201 ACFI, é um Violão Clássico da linha Estudante,
+                construido com tampo de Marfim Laminado, laterais e fundo de
+                Imbuia Laminada e braço em Cedro com escala de Purple Heart.
+                Essa seleção de materiais unidos a uma boa construção,
+                proporcionam ao instrumento um timbre versátil e equilibrado, o
+                que torna o RX201 ACFI a escolha perfeita para seus estudos!
               </p>
             </div>
           </div>
